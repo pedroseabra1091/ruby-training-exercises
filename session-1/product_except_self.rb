ea11df5142dabe4bca617a7bbca24e9f248a43d8
pedroseabra1091 <pedroseabra1091@gmail.com>
2024-02-21 14:10:56 +0000

    New constraints on product_except_self.rb

--- session-1/product_except_self.rb
+++ session-1/product_except_self.rb
@@ -1,11 +1,15 @@
 def product_except_self(nums)
+  length = nums.length
+  raise ArgumentError, "Invalid length of nums array" unless (2..10**5).cover?(length)
+
   answer = [1]
-  (1..nums.length - 1).each do |i|
+  (1..length - 1).each do |i|
     answer[i] = answer[i-1] * nums[i-1]
   end
 
   r = 1
-  ((nums.length - 1).downto(0)).each do |i|
+  ((length - 1).downto(0)).each do |i|
+    raise ArgumentError, "Invalid value in nums array" unless (-30..30).cover?(nums[i])
     answer[i] *= r
     r *= nums[i]
   end
